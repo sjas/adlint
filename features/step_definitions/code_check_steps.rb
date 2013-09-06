4b8b126618f784de9ad8d9f0a729c55a5fea8024
Yutaka Yanoh <yanoh@users.sourceforge.net>
2013-09-06 14:16:48 +0900

    * features/step_definitions/code_check_steps.rb : Fix useless method   call in spec validation of the Cucumber step definition.

--- features/step_definitions/code_check_steps.rb
+++ features/step_definitions/code_check_steps.rb
@@ -8,7 +8,7 @@ When /^I successfully run `(.*)` on noarch$/ do |abbrev_cmd|
 end
 
 Then /^the output should exactly match with:$/ do |mesg_table|
-  $all_output.lines.to_a.size.should == mesg_table.hashes.size
+  $all_output.lines.size.should == mesg_table.hashes.size
   $all_output.each_line.zip(mesg_table.hashes).each do |line, row|
     if row
       line.should =~ /#{row[:line]}:#{row[:column]}:.*:.*:#{row[:mesg]}/
